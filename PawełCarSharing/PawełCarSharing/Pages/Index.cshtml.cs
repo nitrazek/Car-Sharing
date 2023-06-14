56c2446c94d5a348577c58239617f05c986d1523
nitrazek <99548252+nitrazek@users.noreply.github.com>
2023-06-14 22:24:36 +0200

    Próby stworzenia strony logowania

--- PawełCarSharing/PawełCarSharing/Pages/Index.cshtml.cs
+++ PawełCarSharing/PawełCarSharing/Pages/Index.cshtml.cs
@@ -5,16 +5,13 @@ namespace PawełCarSharing.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
-
-        public IndexModel(ILogger<IndexModel> logger)
-        {
-            _logger = logger;
-        }
-
         public void OnGet()
         {
 
         }
     }
 }
+
+
+
+
